7837d488401bb9a062b888c45ab91b1907300c4d
Erick Guzman <guzmi.dev@gmail.com>
2022-03-11 20:36:12 -0700

    :art: private chat and pubic chat

--- public/js/chat.js
+++ public/js/chat.js
@@ -45,18 +45,62 @@ const conectarSocket = async () => {
     console.log("Sockets offline");
   });
 
-  socket.on("recibir-mensaje", () => {
-    // TODO:
-  });
+  socket.on("recibir-mensaje", dibujarMensajes);
 
-  socket.on("usuarios-activos", () => {
-    // TODO:
-  });
+  socket.on("usuarios-activos", dibujarUsuarios);
 
-  socket.on("mensaje-privado", () => {
-    // TODO:
+  socket.on("mensaje-privado", (payload) => {
+    console.log(payload);
   });
 };
+
+const dibujarUsuarios = (usuarios = []) => {
+  let userHtml = "";
+  usuarios.forEach(({ nombre, uid }) => {
+    userHtml += `
+      <li>
+        <p>
+          <h5 class="text-success">${nombre}</h5>
+          <span class="fs-6 text-muted">${uid}</span>
+        </p>
+      </li>
+    `;
+  });
+
+  ulUsuario.innerHTML = userHtml;
+};
+
+const dibujarMensajes = (mensajes = []) => {
+  let mensajesHTML = "";
+  mensajes.forEach(({ nombre, mensaje }) => {
+    mensajesHTML += `
+      <li>
+        <p>
+          <span class="text-primary">${nombre}</span>
+          <span >${mensaje}</span>
+        </p>
+      </li>
+    `;
+  });
+
+  ulMensajes.innerHTML = mensajesHTML;
+};
+
+txtMensaje.addEventListener("keyup", ({ keyCode }) => {
+  const mensaje = txtMensaje.value;
+  const uid = txtUid.value;
+
+  if (keyCode !== 13) {
+    return;
+  }
+  if (mensaje.length === 0) {
+    return;
+  }
+
+  socket.emit("enviar-mensaje", { mensaje, uid });
+
+  txtMensaje.value = "";
+});
 
 const main = async () => {
   await validarJWT();
--- sockets/controller.js
+++ sockets/controller.js
@@ -1,7 +1,10 @@
 const { Socket } = require("socket.io");
 const { comprobarJWT } = require("../helpers");
+const { ChatMensajes } = require("../models");
 
-const socketController = async (socket = new Socket()) => {
+const chatMensajes = new ChatMensajes();
+
+const socketController = async (socket = new Socket(), io) => {
   const token = socket.handshake.headers["x-token"];
   const usuario = await comprobarJWT(token);
 
@@ -9,7 +12,27 @@ const socketController = async (socket = new Socket()) => {
     return socket.disconnect();
   }
 
-  console.log("Se conectó", usuario.nombre);
+  chatMensajes.conectarUsuario(usuario);
+  io.emit("usuarios-activos", chatMensajes.usuariosArr);
+  socket.emit("recibir-mensaje", chatMensajes.ultimos10);
+
+  // conectarlo a una sala especial
+  socket.join(usuario.id);
+
+  socket.on("disconnect", () => {
+    chatMensajes.desconectarUsuario(usuario.id);
+    io.emit("usuarios-activos", chatMensajes.usuariosArr);
+  });
+
+  socket.on("enviar-mensaje", ({ uid, mensaje }) => {
+    if (uid) {
+      // Mensaje privado
+      socket.to(uid).emit("mensaje-privado", { de: usuario.nombre, mensaje });
+    } else {
+      chatMensajes.enviarMensaje(usuario.id, usuario.nombre, mensaje);
+      io.emit("recibir-mensaje", chatMensajes.ultimos10);
+    }
+  });
 };
 
 module.exports = {
